a284607382bc7aa36730e35f37dfef44de3ff005
Lucas Souza <lucasas@gmail.com>
2015-03-05 17:18:53 -0300

    Enabling anti forgery token for posts

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.2"]
                  [ring/ring-defaults "0.1.2"]
-                 [org.clojure/data.csv "0.1.2"]]
+                 [org.clojure/data.csv "0.1.2"]
+                 [ring-anti-forgery "0.3.0"]
+                 [cheshire "5.4.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler closeness-centrality.handler/app}
   :profiles
--- src/closeness_centrality/handler.clj
+++ src/closeness_centrality/handler.clj
@@ -1,10 +1,18 @@
 (ns closeness-centrality.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [resources not-found]]
+            [compojure.handler :refer [site]]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.anti-forgery :refer :all]
+            [cheshire.core :refer :all]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World"))
+  (GET "/" [] (generate-string {:csrf-token *anti-forgery-token*}))
+  (POST "/edge" [origin, destiny]
+    (str "<h1>Hello user " [(keyword origin) (keyword destiny)] "</h1>")))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (->
+   (site app-routes)
+   (wrap-anti-forgery)
+   (wrap-session)))
