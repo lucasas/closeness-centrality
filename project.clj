227e46267378484867f61e750c1c1c03b2c73a9f
Lucas Souza <lucasas@gmail.com>
2015-03-05 12:10:46 -0300

    Simple endpoint in order to verify if compojure is working

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"] [compojure "1.3.2"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.2"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler closeness-centrality.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
---
+++ src/closeness_centrality/handler.clj
@@ -0,0 +1,10 @@
+(ns closeness-centrality.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
