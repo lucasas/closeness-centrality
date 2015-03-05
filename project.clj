947da2096cbd4fd47d4c22c7bb7e53e3e80bc314
Lucas Souza <lucasas@gmail.com>
2015-03-05 15:35:46 -0300

    function which loads all edges included in resources/edges

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.2"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [org.clojure/data.csv "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler closeness-centrality.handler/app}
   :profiles
