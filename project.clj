abb6cd55d7eb2cf3838157a3543170c1a4bc780a
Lucas Souza <lucasas@gmail.com>
2015-03-07 15:32:30 -0300

    Using pedestal because I was facing problems with ring anti forgery token

--- project.clj
+++ project.clj
@@ -4,13 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.2"]
-                 [ring/ring-defaults "0.1.2"]
+                 [io.pedestal/pedestal.service "0.3.1"]
+                 [io.pedestal/pedestal.jetty "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.7"]
+                 [org.slf4j/jcl-over-slf4j "1.7.7"]
+                 [org.slf4j/log4j-over-slf4j "1.7.7"]
                  [org.clojure/data.csv "0.1.2"]
-                 [ring-anti-forgery "0.3.0"]
                  [cheshire "5.4.0"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler closeness-centrality.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "closeness-centrality.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.3.0"]]}}
+  :main ^{:skip-aot true} closeness-centrality.server)
